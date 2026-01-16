2840197bebe7d4892ed92fd5d63268640e5da8a1
wllmgjsn <william.gijsen@student.vinci.be>
2026-01-16 15:56:51 +0100

    Initiated node

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+  res.send('Hello TypeScript + Express!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur lancé sur http://localhost:${port}`);
+});
+
+export default app;
